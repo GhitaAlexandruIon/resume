14a50d34a3b6bafca010c3639ac2d2e42a24ed3d
Adrian Helerea <adrian.helerea@gmail.com>
2019-07-22 18:24:26 +0300

    fix paths for login and logout

--- user_registration/urls.py
+++ user_registration/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from user_registration import views as app1_views
-from django.contrib.auth import views as auth_views
+from django.contrib.auth import views
 from django.urls import path
 from django.views.generic.base import TemplateView
 
@@ -13,8 +13,8 @@ urlpatterns = [
     # url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
     # url(r'^admin/', admin.site.urls),
     url(r'^$', TemplateView.as_view(template_name='app1/index.html'), name='home'),
-    path('login/', auth_views.LoginView, {'template_name': 'registration/login.html'}, name='login'),
-    url(r'^logout/$', auth_views.LogoutView, {'template_name': 'logged_out.html'}, name='logout'),
+    # path('login/', auth_views.LoginView, {'template_name': 'registration/login.html'}, name='login'),
+    # url(r'^logout/$', auth_views.LogoutView, {'template_name': 'logged_out.html'}, name='logout'),
     # url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
     # url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
     # url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
@@ -22,5 +22,7 @@ urlpatterns = [
     # url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
     # url(r'^signup/$', app1_views.signup, name='signup'),
     path('', index, name='index'),
+    path('login/', views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', views.LogoutView.as_view(template_name='registration/logged_out.html'), name='logout'),
 
 ]
