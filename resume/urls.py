f92809a103f7910fe0b04983fc719dce2c1c3164
alex <ghita.alexandru6@gamil.com>
2019-07-26 23:26:13 +0300

    created accounts app for signup

--- resume/urls.py
+++ resume/urls.py
@@ -22,7 +22,8 @@ urlpatterns = [
 
     # neww
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('', TemplateView.as_view(template_name='index.html'), name='home'),
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    #path('', TemplateView.as_view(template_name='index.html'), name='home'),
+    #path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
