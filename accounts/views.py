e0c7ee2484ae640bdffc1705c6b55e619a43a271
alex <ghita.alexandru6@gamil.com>
2019-08-12 10:36:21 +0300

    created registration form

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
 from django.views import generic
 
@@ -8,4 +8,17 @@ class SignUp(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
-# Create your views here.
+    # register views.
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/account')
+    else:
+        form = UserCreationForm()
+
+        args = {'form': form}
+        return render(request, 'accounts/reg_form.html', args)
